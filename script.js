1174cd2216e31a1bcefc8be2eca583b2bb680b31
Anukarsh <anukarshr@gmail.com>
2023-02-24 19:55:44 +0530

    update

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ const options = {
 };
 
 fetch(
-  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle",
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi",
   options
 )
   .then((response) => response.json())
   .then((response) => {
     console.log(response);
-    cloud_pct = response.cloud_pct;
-    temp = response.temp;
-    feels_like = response.feels_like;
-    humidity = response.humidity;
-    min_temp = response.min_temp;
-    max_temp = response.max_temp;
-    wind_speed = response.wind_speed;
-    wind_degrees = response.wind_degrees;
-    sunrise = response.sunrise;
-    sunset = response.sunset;
+    cloud_pct.innerHTML = response.cloud_pct;
+    temp.innerHTML = response.temp;
+    feels_like.innerHTML = response.feels_like;
+    humidity.innerHTML = response.humidity;
+    min_temp.innerHTML = response.min_temp;
+    max_temp.innerHTML = response.max_temp;
+    wind_speed.innerHTML = response.wind_speed;
+    wind_degrees.innerHTML = response.wind_degrees;
+    sunrise.innerHTML = response.sunrise;
+    sunset.innerHTML = response.sunset;
   })
   .catch((err) => console.error(err));
