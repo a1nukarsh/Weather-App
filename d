e2e7d52c8b4f2347cf844df5d669a8dcedca7b89
Anukarsh <anukarshr@gmail.com>
2023-02-24 19:07:13 +0530

    update

--- d
+++ d
@@ -1,13 +1,12 @@
 
-{
-  cloud_pct 50,
-  temp 18,
-  feels_like 18,
-  humidity 94,
-  min_temp 18,
-  max_temp 18,
-  wind_speed 0,
-  wind_degrees 0,
-  sunrise 1676855358,
-  sunset 1676896277
-}
+
+cloud_pct 
+temp 
+feels_like 
+humidity 
+min_temp 
+max_temp 
+wind_speed 
+wind_degrees 
+sunrise 
+sunset 
--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a444819cf0msh03f52708c537cd0p124376jsnd74bf1f8b08d',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a444819cf0msh03f52708c537cd0p124376jsnd74bf1f8b08d",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
 };
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle', options)
-	.then(response => response.json())
-	.then(response => {
-		
-		
-	
-		console.log(response)
-	
-	
-	})
-	.catch(err => console.error(err));
+fetch(
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    cloud_pct = response.cloud_pct;
+    temp = response.temp;
+    feels_like = response.feels_like;
+    humidity = response.humidity;
+    min_temp = response.min_temp;
+    max_temp = response.max_temp;
+    wind_speed = response.wind_speed;
+    wind_degrees = response.wind_degrees;
+    sunrise = response.sunrise;
+    sunset = response.sunset;
+  })
+  .catch((err) => console.error(err));
